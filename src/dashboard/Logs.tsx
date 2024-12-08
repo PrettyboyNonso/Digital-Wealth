3555ced62a3a1c59a50f1dc32f72112642034764
Darlington <obidarlington09@gmail.com>
2024-12-08 19:16:51 +0100

    Little UI tweek

--- src/dashboard/Logs.tsx
+++ src/dashboard/Logs.tsx
@@ -1,22 +1,49 @@
 import { BanknoteIcon } from "lucide-react";
+import React from "react";
 
 const Logs = () => {
-  const LogCard = () => {
+  interface LogsType {
+    date: string;
+    amount: string;
+    success: boolean;
+  }
+  const LogsDet = [
+    {
+      date: "tue-8th-24",
+      amount: "0.00034btc",
+      success: true,
+    },
+    {
+      date: "thur-10th-24",
+      amount: "0.00634btc",
+      success: false,
+    },
+    {
+      date: "fri-11th-24",
+      amount: "0.00504btc",
+      success: true,
+    },
+  ];
+  const LogCard: React.FC<{ value: LogsType }> = ({ value }) => {
     return (
-      <div className="w-full py-2 flex gap-4 items-start">
+      <div className="w-full py-2 flex gap-4 items-start border-b border-solid">
         <div className="flex-shrink-0 flex-grow-0 w-[8%]">
           <BanknoteIcon className="w-full h-full" />
         </div>
 
         <div className="flex-shrink-0 flex-grow-0 min-w-[30%] max-w-fit font-mons capitalize flex flex-col">
-          <p className="font-bold text-xs">deposit</p>
-          <p className=" font-medium text-xs">tue-8th-24</p>
+          <p className="font-bold text-xs text-teal-700 uppercase">deposit</p>
+          <p className=" font-medium text-xs">{value.date}</p>
         </div>
 
         <div className="items-end flex-shrink-0 flex-grow-0 w-[55%]  font-mons capitalize flex flex-col">
-          <p className="font-bold text-xs">0.00034btc</p>
-          <p className="text-xs px-1 rounded-sm bg-green-600 font-bold text-white">
-            success
+          <p className="font-bold text-xs">{value.amount}</p>
+          <p
+            className={`text-xs px-1 rounded-sm  font-semibold text-white ${
+              value.success ? "bg-green-600" : "bg-red-600"
+            }`}
+          >
+            {value.success ? "success" : "failed"}
           </p>
         </div>
       </div>
@@ -24,9 +51,9 @@ const Logs = () => {
   };
   return (
     <div className="w-full flex flex-col py-4 px-4 gap-4">
-      <LogCard />
-      <LogCard />
-      <LogCard />
+      {LogsDet.map((item, index) => (
+        <LogCard value={item} key={index} />
+      ))}
     </div>
   );
 };
