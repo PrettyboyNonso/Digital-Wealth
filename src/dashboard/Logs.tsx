51fd7c523f65d1a9b169915f6bb00de65178beb6
Darlington <obidarlington09@gmail.com>
2024-12-09 18:15:34 +0100

    Checking --v2

--- src/dashboard/Logs.tsx
+++ src/dashboard/Logs.tsx
@@ -1,10 +1,19 @@
+import { FileText } from "lucide-react";
+
 const Logs = () => {
   const LogsCard = () => {
-    return <div></div>;
+    return (
+      <div className="flex items-center mt-7">
+        <div className="flex-shrink-0 flex-grow-0 flex items-center gap-1 basis-[50%] ">
+          <FileText className="" />
+          <p className="font-mono font-semibold capitalize">invoice_001</p>
+        </div>
+      </div>
+    );
   };
   return (
     <div className="px-3 py-3">
-      <h1 className="font-mono font-semibold capitalize text-sm">
+      <h1 className="font-mono font-semibold uppercase text-sm">
         fund history
       </h1>
 
