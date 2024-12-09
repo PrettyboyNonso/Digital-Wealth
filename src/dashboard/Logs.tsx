7ab2fa85844231516c363539cce5bd9b2894177a
Darlington <obidarlington09@gmail.com>
2024-12-09 18:26:42 +0100

    Checking --v4

--- src/dashboard/Logs.tsx
+++ src/dashboard/Logs.tsx
@@ -6,16 +6,16 @@ const Logs = () => {
       <div className="flex items-center border border-solid py-4 px-3 shadow-md rounded-sm bg-blue-500">
         <div className="flex-shrink-0 flex-grow-0 flex items-center gap-1 basis-[50%] ">
           <FileText className="text-white" />
-          <p className="font-mono font-semibold capitalize text-sm text-white">
+          <p className="font-mono font-semibold capitalize text-xs text-white">
             invoice_001
           </p>
         </div>
 
         <div className="flex-shrink-0 flex-grow-0 flex items-center justify-between basis-[50%]">
-          <p className="font-mono font-semibold capitalize text-sm text-white">
+          <p className="font-mono font-semibold capitalize text-xs text-white">
             12 apr 2025
           </p>
-          <p className="font-mono font-bold capitalize text-sm text-white">
+          <p className="font-mono font-bold capitalize text-xs text-white">
             USD 100
           </p>
         </div>
